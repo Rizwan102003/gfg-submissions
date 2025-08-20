b2ef41b3faa1ff54a245b69bfa4b92edc4839d51
Sk Md Rizwan <skmdrizwan2003@gmail.com>
2025-08-20 23:20:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in fully rotated sorted 2D matrix/search-in-fully-rotated-sorted-2d-matrix.py
@@ -0,0 +1,8 @@
+class Solution:
+    def searchMatrix(self, mat, x):
+        # code here
+        for lst in mat:
+            if x in lst:
+                return True
+                
+        return False
