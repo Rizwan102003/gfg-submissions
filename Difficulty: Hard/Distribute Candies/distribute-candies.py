d87f9353e174d5cfa988fd38c42ab7b863f49910
Sk Md Rizwan <skmdrizwan2003@gmail.com>
2025-10-12 21:39:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Distribute Candies/distribute-candies.py
@@ -0,0 +1,22 @@
+'''
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.right = None
+        self.left = None
+'''
+
+class Solution:
+    ans = 0
+    
+    def solve(self, root):
+        if not root: return 0
+        left = self.solve(root.left)
+        right = self.solve(root.right)
+        ttl = left+right+root.data-1
+        self.ans += abs(ttl)
+        return ttl
+    
+    def distCandy(self, root):
+        self.solve(root)
+        return self.ans
