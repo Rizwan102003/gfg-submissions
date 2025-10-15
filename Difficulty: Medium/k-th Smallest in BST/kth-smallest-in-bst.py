4f2534c4d3e9254087e09583affb8e44c3e1c6bb
Sk Md Rizwan <skmdrizwan2003@gmail.com>
2025-10-15 13:02:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/k-th Smallest in BST/kth-smallest-in-bst.py
@@ -0,0 +1,24 @@
+'''
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+
+class Solution:
+    def kthSmallest(self, root, k): 
+        # code here
+        ret=-1
+        def dfs(cur=root):
+            nonlocal k,ret
+            if not cur:
+                return
+            dfs(cur.left)
+            k-=1
+            if k==0:
+                ret=cur.data
+                return
+            dfs(cur.right)
+        dfs()
+        return ret
